5ad515dca14f70a7a04eaf1c394e740ef515d091
Banh Trung <trungbanh97@gmail.com>
2021-11-28 22:32:32 +0700

    first commit

---
+++ app/Http/Requests/ApiFormRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Response;
+use \Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ApiFormRequest extends FormRequest
+{
+    protected function failedValidation(Validator $validator)
+    {
+
+        $errors = (new ValidationException($validator))->errors();
+
+        throw new HttpResponseException(
+            response()->json(['errors' => $errors], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/unauthenticated', 'AuthController@unauthenticated')->name('unauthenticated');
+Route::post('/login', 'AuthController@login');
+Route::post('/register', 'AuthController@register');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/users', 'UserController@index');
+
+    Route::post('/logout', 'AuthController@logout');
+});
